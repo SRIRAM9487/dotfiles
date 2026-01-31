2a53de1ce46dca94cd3eb0b071c0faf449281a82
SRIRAM9487 <sriram9487tk@gmail.com>
2026-01-31 17:56:24 +0530

    rofi config added and nvim cmp updated

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -35,6 +35,17 @@ return {
 			enabled = function()
 				local context = require("cmp.config.context")
 
+				-- Disable in prompt-like buffers and file explorers
+				local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+				if buftype == "prompt" then
+					return false
+				end
+
+				local filetype = vim.bo.filetype
+				if filetype == "neo-tree" or filetype == "neo-tree-popup" then
+					return false
+				end
+
 				-- Always enable in command mode
 				if vim.api.nvim_get_mode().mode == "c" then
 					return true
